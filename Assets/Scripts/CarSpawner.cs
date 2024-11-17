82ec7a2b7f1505bcc637b481343e34b28f3c792a
IntactoGit <intacto1996@gmail.com>
2024-11-17 14:48:13 +0300

    initial commit

---
+++ Assets/Scripts/CarSpawner.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEngine;
+
+public class CarSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _prefab;
+
+    [SerializeField]
+    private Transform _parent;
+    private void Awake()
+    {
+        Instantiate(_prefab, _parent);
+    }
+}
